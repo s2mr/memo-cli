66540579cc30be7c113f2deae5fdcb0468ed7334
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2023-02-15 00:24:19 +0900

    Fix scripts

---
+++ .github/workflows/my-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git config user.name s2mr
+git config user.email s2mr@users.noreply.github.com
+BODY="${{github.event.comment.body}}"
+TARGET_BRANCH="$(sed -e 's/\/ota --with \(.*\) .*/\1/g')"
+
+if [ "$TARGET_BRANCH" = "" ]
+then
+    git checkout "$TARGET_BRANCH"
+fi
+
+git commit --allow-empty -m "[ota]${BODY##/ota}"
+git push
