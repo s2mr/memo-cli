980592aa8479152c38b04d656f0cd26b501b87f1
s2mr <s2mr@users.noreply.github.com>
2023-02-15 01:32:24 +0900

    Fix

--- Scripts/my-script.sh
+++ Scripts/my-script.sh
@@ -6,12 +6,10 @@ git config user.name s2mr
 git config user.email s2mr@users.noreply.github.com
 TARGET_BRANCH=$(echo "$COMMENT_BODY" | sed -e 's/\/ota --with \(.*\).*/\1/g')
 
-git pull
-
 if [ "$TARGET_BRANCH" != "" ]
 then
-    git fetch "$TARGET_BRANCH"
-    git checkout origin/"$TARGET_BRANCH"
+    git pull
+    git checkout "$TARGET_BRANCH"
 fi
 
 git commit --allow-empty -m "[ota]${COMMENT_BODY##/ota}"
