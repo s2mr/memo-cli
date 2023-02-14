c62c9a2cc414ea5f08f81e56b52715999c76178f
s2mr <s2mr@users.noreply.github.com>
2023-02-15 01:07:43 +0900

    WIP

--- Scripts/my-script.sh
+++ Scripts/my-script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+set -eu
+
 git config user.name s2mr
 git config user.email s2mr@users.noreply.github.com
-BODY="${{github.event.comment.body}}"
-TARGET_BRANCH="$(sed -e 's/\/ota --with \(.*\) .*/\1/g')"
+TARGET_BRANCH="$(echo "$COMMENT_BODY" | sed -e 's/\/ota --with \(.*\) .*/\1/g')"
 
-if [ "$TARGET_BRANCH" = "" ]
+if [ "$TARGET_BRANCH" != "" ]
 then
     git checkout "$TARGET_BRANCH"
 fi
 
-git commit --allow-empty -m "[ota]${BODY##/ota}"
+git commit --allow-empty -m "[ota]${COMMENT_BODY##/ota}"
 git push
