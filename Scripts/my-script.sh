8202f6a963e440f01baded1c4aa01477173753b0
s2mr <s2mr@users.noreply.github.com>
2023-02-15 01:11:31 +0900

    fix

--- Scripts/my-script.sh
+++ Scripts/my-script.sh
@@ -6,6 +6,10 @@ git config user.name s2mr
 git config user.email s2mr@users.noreply.github.com
 TARGET_BRANCH="$(echo "$COMMENT_BODY" | sed -e 's/\/ota --with \(.*\) .*/\1/g')"
 
+echo "$TARGET_BRANCH"
+git status
+git branch
+
 if [ "$TARGET_BRANCH" != "" ]
 then
     git checkout "$TARGET_BRANCH"
