6d534998cc86f6936fdae8574ba0d6f5b2384658
s2mr <s2mr@users.noreply.github.com>
2023-02-16 01:13:48 +0900

    Fix

--- Scripts/my-script.sh
+++ Scripts/my-script.sh
@@ -17,7 +17,7 @@ TARGET_BRANCH=$(echo "$COMMENT_BODY" | sed 's/.*--into *\([^ ]*\).*/\1/')
 if [ "$TARGET_BRANCH" != "" ]; then
     git pull 2> /tmp/Error
     git checkout "$TARGET_BRANCH" 2> /tmp/Error
-    git merge origin/"$PR_BRANCH" --no-ff 2> /tmp/Error
+    git merge origin/"$PR_BRANCH" --no-ff -m "Merge remote-tracking branch 'origin/$PR_BRANCH' into $TARGET_BRANCH" 2> /tmp/Error
 else
     git commit --allow-empty -m "[ota]${COMMENT_BODY##/ota}" 2> /tmp/Error
 fi
